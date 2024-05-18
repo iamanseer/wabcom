a4c9028de94112a7c1e16e9696ad3fc0e4e87f25
Dinshashanoj <dinsha.progbiz@gmail.com>
2024-05-18 16:39:47 +0400

    Updates

--- PB/Shared/Models/CRM/Customer/CustomerModelNew.cs
+++ PB/Shared/Models/CRM/Customer/CustomerModelNew.cs
@@ -38,7 +38,7 @@ namespace PB.Shared.Models.CRM.Customer
         public bool? AllowType { get; set; }
         public int? ContactID { get; set; }
         // [Required(ErrorMessage = "Please choose a country")]
-        public int? CountryID { get; set; } 
+        public int? CountryID { get; set; }
         public string? CountryName { get; set; }
         public string? ISDCode { get; set; }
         public DateTime? JoinedOn { get; set; } = DateTime.UtcNow;
@@ -75,15 +75,8 @@ namespace PB.Shared.Models.CRM.Customer
     public class CustomerSerialNoModel
     {
         public int ID { get; set; }
-        [Required(ErrorMessage = "Please add tally serial no")]
-        [StringLength(maximumLength: 9, ErrorMessage = "Length must be between 0 and 9 characters")]
-        [RegularExpression(@"^[a-z0-9-]+$", ErrorMessage = "tally serial no field should only contain white spaces,small letters,numbers and hyphens")]
         public string? TallySerialNo { get; set; } = "0";
-        [StringLength(maximumLength: 9, ErrorMessage = "Length must be between 0 and 9 characters")]
-        [RegularExpression(@"^[a-z0-9-]+$", ErrorMessage = "tally serial no2 field should only contain white spaces,small letters,numbers and hyphens")]
         public string? TallySerialNo2 { get; set; } = "0";
-        [StringLength(maximumLength: 9, ErrorMessage = "Length must be between 0 and 9 characters")]
-        [RegularExpression(@"^[a-z0-9-]+$", ErrorMessage = "tally serial no3 field should only contain white spaces,small letters,numbers and hyphens")]
         public string? TallySerialNo3 { get; set; } = "0";
         public int? CustomerID { get; set; }
     }
--- PB/Shared/Tables/Common/CustomerSerialNumbers.cs
+++ PB/Shared/Tables/Common/CustomerSerialNumbers.cs
@@ -12,16 +12,9 @@ namespace PB.Shared.Tables.Common
     {
         [PrimaryKey]
         public int ID { get; set; }
-        [Required(ErrorMessage = "Please add tally serial no")]
-        [StringLength(maximumLength: 9, ErrorMessage = "Length must be between 0 and 9 characters")]
-        [RegularExpression(@"^[a-z0-9-]+$", ErrorMessage = "tally serial no field should only contain white spaces,small letters,numbers and hyphens")]
-        public string? TallySerialNo { get; set; }
-        [StringLength(maximumLength: 9, ErrorMessage = "Length must be between 0 and 9 characters")]
-        [RegularExpression(@"^[a-z0-9-]+$", ErrorMessage = "tally serial no2 field should only contain white spaces,small letters,numbers and hyphens")]
+       public string? TallySerialNo { get; set; }
         public string? TallySerialNo2 { get; set; }
-        [StringLength(maximumLength: 9, ErrorMessage = "Length must be between 0 and 9 characters")]
-        [RegularExpression(@"^[a-z0-9-]+$", ErrorMessage = "tally serial no3 field should only contain white spaces,small letters,numbers and hyphens")]
-        public string? TallySerialNo3 { get; set; }
+       public string? TallySerialNo3 { get; set; }
         public int? CustomerID { get; set; }
     }
 }
